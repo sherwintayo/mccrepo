5e3b9a62e7bb7c056d07c1c2f4c4b06921636dbb
sherwintayo <sherwintayo08@gmail.com>
2024-09-14 15:01:55 +0800

    .

--- admin/forgot_password.php
+++ admin/forgot_password.php
@@ -1,7 +1,7 @@
 <?php require_once('../config.php'); ?>
 <!DOCTYPE html>
 <html lang="en" class="" style="height: auto;">
-<?php require_once('inc/header.php') ?>
+<!-- <?php require_once('inc/header.php') ?> -->
 <body class="hold-transition">
   <script>
     start_loader()
