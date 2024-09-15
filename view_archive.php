65795875cea30ac498ae9cfe3c9ed43a8fec55d7
sherwintayo <sherwintayo08@gmail.com>
2024-09-15 23:48:50 +0800

    .

--- view_archive.php
+++ view_archive.php
@@ -1,7 +1,11 @@
 <?php 
 if(isset($_GET['id']) && $_GET['id'] > 0){
-    $qry = $conn->query("SELECT a.* FROM `archive_list` a where a.id = '{$_GET['id']}'");
-    if($qry->num_rows){
+    $stmt = $conn->prepare("SELECT a.* FROM `archive_list` a WHERE a.id = ?");
+    $stmt->bind_param("i", $_GET['id']);
+    $stmt->execute();
+    $qry = $stmt->get_result();
+
+    if($qry->num_rows > 0){
         foreach($qry->fetch_array() as $k => $v){
             if(!is_numeric($k))
             $$k = $v;
@@ -9,15 +13,21 @@ if(isset($_GET['id']) && $_GET['id'] > 0){
     }
     $submitted = "N/A";
     if(isset($student_id)){
-        $student = $conn->query("SELECT * FROM student_list where id = '{$student_id}'");
+        $stmt = $conn->prepare("SELECT * FROM student_list WHERE id = ?");
+        $stmt->bind_param("i", $student_id);
+        $stmt->execute();
+        $student = $stmt->get_result();
+
         if($student->num_rows > 0){
             $res = $student->fetch_array();
             $submitted = $res['email'];
         }
     }
 
-    //record count exec
-    $conn->query("INSERT INTO archive_counter (archive_id) VALUES ({$_GET['id']})");
+    // Record count execution
+    $stmt = $conn->prepare("INSERT INTO archive_counter (archive_id) VALUES (?)");
+    $stmt->bind_param("i", $_GET['id']);
+    $stmt->execute();
 }
 ?>
 <style>
@@ -30,52 +40,48 @@ if(isset($_GET['id']) && $_GET['id'] > 0){
         <div class="card card-outline card-primary shadow rounded-0">
             <div class="card-header">
                 <h3 class="card-title">
-                    Archives - <?= isset($archive_code) ? $archive_code : "" ?>
+                    Archives - <?= isset($archive_code) ? htmlspecialchars($archive_code) : "" ?>
                 </h3>
             </div>
             <div class="card-body rounded-0">
                 <div class="container-fluid">
-                    <h2><b><?= isset($title) ? $title : "" ?></b></h2>
-                    <small class="text-muted">Submitted by <b class="text-info"><?= $submitted ?></b> on  <?= date("F d, Y h:i A",strtotime($date_created)) ?></small>
+                    <h2><b><?= isset($title) ? htmlspecialchars($title) : "" ?></b></h2>
+                    <small class="text-muted">Submitted by <b class="text-info"><?= htmlspecialchars($submitted) ?></b> on <?= isset($date_created) ? date("F d, Y h:i A", strtotime($date_created)) : "" ?></small>
                     <?php if(isset($student_id) && $_settings->userdata('login_type') == "2" && $student_id == $_settings->userdata('id')): ?>
                         <div class="form-group">
-                            <a href="./?page=submit-archive&id=<?= isset($id) ? $id : "" ?>" class="btn btn-flat btn-default bg-navy btn-sm"><i class="fa fa-edit"></i> Edit</a>
-                            <button type="button" data-id = "<?= isset($id) ? $id : "" ?>" class="btn btn-flat btn-danger btn-sm delete-data"><i class="fa fa-trash"></i> Delete</button>
+                            <a href="./?page=submit-archive&id=<?= isset($id) ? htmlspecialchars($id) : "" ?>" class="btn btn-flat btn-default bg-navy btn-sm"><i class="fa fa-edit"></i> Edit</a>
+                            <button type="button" data-id="<?= isset($id) ? htmlspecialchars($id) : "" ?>" class="btn btn-flat btn-danger btn-sm delete-data"><i class="fa fa-trash"></i> Delete</button>
                         </div>
                     <?php endif; ?>
                     <hr>
                     <center>
-                        <img src="<?= validate_image(isset($banner_path) ? $banner_path : "") ?>" alt="Banner Image" id="banner-img" class="img-fluid border bg-gradient-dark">
+                        <img src="<?= validate_image(isset($banner_path) ? htmlspecialchars($banner_path) : "") ?>" alt="Banner Image" id="banner-img" class="img-fluid border bg-gradient-dark">
                     </center>
                     <fieldset>
                         <legend class="text-navy">Project Year:</legend>
-                        <div class="pl-4"><large><?= isset($year) ? $year : "----" ?></large></div>
+                        <div class="pl-4"><large><?= isset($year) ? htmlspecialchars($year) : "----" ?></large></div>
                     </fieldset>
                     <fieldset>
                         <legend class="text-navy">Abstract:</legend>
                         <div class="pl-4">
-                            <large>
-                                <?= isset($abstract) ? html_entity_decode($abstract) : "" ?>
-                        </large>
-                    </div>
+                            <large><?= isset($abstract) ? html_entity_decode($abstract) : "" ?></large>
+                        </div>
                     </fieldset>
                     <fieldset>
                         <legend class="text-navy">Members:</legend>
                         <div class="pl-4"><large><?= isset($members) ? html_entity_decode($members) : "" ?></large></div>
                     </fieldset>
                     <fieldset>
-						<legend class="text-navy">Project Files:</legend>
-						
-						<a class="btn btn-success" href="<?php echo base_url.'uploads/files/Files-'.$id.'.zip' ?>">Download Project files</a>
-						
-					</fieldset>
-					<fieldset>
+                        <legend class="text-navy">Project Files:</legend>
+                        <a class="btn btn-success" href="<?= base_url . 'uploads/files/Files-' . htmlspecialchars($id) . '.zip' ?>">Download Project files</a>
+                    </fieldset>
+                    <fieldset>
                         <legend class="text-navy">SQL file:</legend>
-                        <a class="btn btn-success" href="<?php echo base_url.'uploads/sql/SQL-'.$id.'.sql' ?>">Download SQL file</a>
+                        <a class="btn btn-success" href="<?= base_url . 'uploads/sql/SQL-' . htmlspecialchars($id) . '.sql' ?>">Download SQL file</a>
                     </fieldset>
                     <fieldset>
                         <legend class="text-navy">Project Document:</legend>
-                        <a class="btn btn-success" href="<?php echo base_url.'uploads/pdf/Document-'.$id.'.pdf' ?>">Download Project Document</a>
+                        <a class="btn btn-success" href="<?= base_url . 'uploads/pdf/Document-' . htmlspecialchars($id) . '.pdf' ?>">Download Project Document</a>
                     </fieldset>
                 </div>
             </div>
@@ -85,42 +91,41 @@ if(isset($_GET['id']) && $_GET['id'] > 0){
 <script>
     $(function(){
         $('.delete-data').click(function(){
-            _conf("Are you sure to delete <b>Archive-<?= isset($archive_code) ? $archive_code : "" ?></b>","delete_archive")
-        })
-		$('.summernote').summernote({
+            _conf("Are you sure to delete <b>Archive-<?= isset($archive_code) ? htmlspecialchars($archive_code) : "" ?></b>", "delete_archive")
+        });
+        $('.summernote').summernote({
             height: 200
-            
-        })
-		$("#summernote").summernote("disable");
-    })
+        });
+        $("#summernote").summernote("disable");
+    });
+
     function delete_archive(){
-		start_loader();
-		$.ajax({
-			url:_base_url_+"classes/Master.php?f=delete_archive",
-			method:"POST",
-			data:{id: "<?= isset($id) ? $id : "" ?>"},
-			dataType:"json",
-			error:err=>{
-				console.log(err)
-				alert_toast("An error occured.",'error');
-				end_loader();
-			},
-			success:function(resp){
-				if(typeof resp== 'object' && resp.status == 'success'){
+        start_loader();
+        $.ajax({
+            url: _base_url_ + "classes/Master.php?f=delete_archive",
+            method: "POST",
+            data: {id: "<?= isset($id) ? htmlspecialchars($id) : "" ?>"},
+            dataType: "json",
+            error: err => {
+                console.log(err);
+                alert_toast("An error occurred.", 'error');
+                end_loader();
+            },
+            success: function(resp){
+                if(typeof resp == 'object' && resp.status == 'success'){
                     Swal.fire({
                         icon: 'success',
                         title: 'Deleted Successfully',
-                        // text: 'Click OK to Continue.',
                         showConfirmButton: false,
                         timer: 1000
                     }).then(() => {
                         location.replace("./");
-                    })
-				}else{
-					alert_toast("An error occured.",'error');
-					end_loader();
-				}
-			}
-		})
-	}
+                    });
+                } else {
+                    alert_toast("An error occurred.", 'error');
+                    end_loader();
+                }
+            }
+        });
+    }
 </script>
