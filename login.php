b5616c69f8d8b0059805dad1b57123df7a6d7b83
sherwintayo <sherwintayo08@gmail.com>
2024-09-17 02:11:50 +0800

    .

--- login.php
+++ login.php
@@ -1,165 +1,101 @@
-<?php 
-if(isset($_GET['id']) && $_GET['id'] > 0){
-    $stmt = $conn->prepare("SELECT a.* FROM `archive_list` a WHERE a.id = ?");
-    $stmt->bind_param("i", $_GET['id']);
-    $stmt->execute();
-    $qry = $stmt->get_result();
+<?php require_once('./config.php') ?>
+<?php session_start(); // Start session handling ?>
+<!DOCTYPE html>
+<html lang="en" class="" style="height: auto;">
+ <?php require_once('inc/header.php') ?>
+<body class="hold-transition ">
+  <script>
+    start_loader()
+  </script>
 
-    if($qry->num_rows > 0){
-        foreach($qry->fetch_array() as $k => $v){
-            if(!is_numeric($k))
-            $$k = $v;
-        }
-    }
-    $submitted = "N/A";
-    if(isset($student_id)){
-        $stmt = $conn->prepare("SELECT * FROM student_list WHERE id = ?");
-        $stmt->bind_param("i", $student_id);
-        $stmt->execute();
-        $student = $stmt->get_result();
-
-        if($student->num_rows > 0){
-            $res = $student->fetch_array();
-            $submitted = $res['email'];
-        }
-    }
-
-    // Record count execution
-    $stmt = $conn->prepare("INSERT INTO archive_counter (archive_id) VALUES (?)");
-    $stmt->bind_param("i", $_GET['id']);
-    $stmt->execute();
-}
-?>
-<style>
-    #document_field{
-        min-height:80vh
-    }
-</style>
-<div class="content py-4">
-    <div class="col-12">
-        <div class="card card-outline card-primary shadow rounded-0">
-            <div class="card-header">
-                <h3 class="card-title">
-                    Archives - <?= isset($archive_code) ? htmlspecialchars($archive_code) : "" ?>
-                </h3>
-            </div>
-            <div class="card-body rounded-0">
-                <div class="container-fluid">
-                    <h2><b><?= isset($title) ? htmlspecialchars($title) : "" ?></b></h2>
-                    <small class="text-muted">Submitted by <b class="text-info"><?= htmlspecialchars($submitted) ?></b> on <?= isset($date_created) ? date("F d, Y h:i A", strtotime($date_created)) : "" ?></small>
-                    <?php if(isset($student_id) && $_settings->userdata('login_type') == "2" && $student_id == $_settings->userdata('id')): ?>
-                        <div class="form-group">
-                            <a href="./?page=submit-archive&id=<?= isset($id) ? htmlspecialchars($id) : "" ?>" class="btn btn-flat btn-default bg-navy btn-sm"><i class="fa fa-edit"></i> Edit</a>
-                            <button type="button" data-id="<?= isset($id) ? htmlspecialchars($id) : "" ?>" class="btn btn-flat btn-danger btn-sm delete-data"><i class="fa fa-trash"></i> Delete</button>
+  <!-- Your login page styles here -->
+  
+  <div class=" d-flex flex-column align-items-center w-100" id="login">
+    <div class="body d-flex flex-column justify-content-center align-items-center">
+     <div class="w-100">
+        <h1 class="text-center py-5 my-5 login-title"><b><?php echo $_settings->info('name') ?></b></h1>
+      </div> 
+        <div class="row myContainer">
+            <div class="myLoginForm col-lg-7 p-3 w-100 d-flex justify-content-center align-items-center text-navy">
+                <div class="d-flex flex-column w-100 px-3">
+                    <h1 class="text-center font-weight-bold text-white">Sign in to Account</h1>
+                    <hr class="my-3" />
+                    <form action="" id="slogin-form">
+                        <div class="input-group form-group">
+                            <input type="email" name="email" id="email" placeholder="Email" class="form-control" required>
                         </div>
-                    <?php endif; ?>
-                    <hr>
-                    <center>
-                        <img src="<?= validate_image(isset($banner_path) ? htmlspecialchars($banner_path) : "") ?>" alt="Banner Image" id="banner-img" class="img-fluid border bg-gradient-dark">
-                    </center>
-                    <fieldset>
-                        <legend class="text-navy">Project Year:</legend>
-                        <div class="pl-4"><large><?= isset($year) ? htmlspecialchars($year) : "----" ?></large></div>
-                    </fieldset>
-                    <fieldset>
-                        <legend class="text-navy">Abstract:</legend>
-                        <div class="pl-4">
-                            <large><?= isset($abstract) ? html_entity_decode($abstract) : "" ?></large>
+                        <div class="input-group form-group">
+                            <input type="password" name="password" id="password" placeholder="Password" class="form-control" required>
                         </div>
-                    </fieldset>
-                    <fieldset>
-                        <legend class="text-navy">Members:</legend>
-                        <div class="pl-4"><large><?= isset($members) ? html_entity_decode($members) : "" ?></large></div>
-                    </fieldset>
-                    <fieldset>
-                        <legend class="text-navy">Project Files:</legend>
-                        <a class="btn btn-success" href="login.php?redirect=download&file_type=zip&id=<?= htmlspecialchars($id) ?>">Download Project files</a>
-                    </fieldset>
-                    <fieldset>
-                        <legend class="text-navy">SQL file:</legend>
-                        <a class="btn btn-success" href="login.php?redirect=download&file_type=sql&id=<?= htmlspecialchars($id) ?>">Download SQL file</a>
-                    </fieldset>
-                    <fieldset>
-                        <legend class="text-navy">Project Document:</legend>
-                        <a class="btn btn-success" href="login.php?redirect=download&file_type=pdf&id=<?= htmlspecialchars($id) ?>">Download Project Document</a>
-                    </fieldset>
-                </div>
+                        <div class="form-group text-right">
+                            <button class="btnLogin btn btn-primary btn-flat text-white">Login</button>
+                        </div>
+                    </form>
+                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<!-- Scripts -->
 <script>
   $(document).ready(function(){
     end_loader();
-    // Registration Form Submit
+    
     $('#slogin-form').submit(function(e){
-        e.preventDefault()
-        var _this = $(this)
-            $(".pop-msg").remove()
-            $('#password, #cpassword').removeClass("is-invalid")
-        var el = $("<div>")
-            el.addClass("alert pop-msg my-2")
-            el.hide()
+        e.preventDefault();
+        var _this = $(this);
+        var el = $("<div>");
+        el.addClass("alert pop-msg my-2").hide();
         start_loader();
+
         $.ajax({
-            url:_base_url_+"classes/Login.php?f=student_login",
-            method:'POST',
-            data:_this.serialize(),
-            dataType:'json',
-            error:err=>{
-                console.log(err)
-                el.text("An error occurred while saving the data")
-                el.addClass("alert-danger")
-                _this.prepend(el)
-                el.show('slow')
+            url: _base_url_ + "classes/Login.php?f=student_login",
+            method: 'POST',
+            data: _this.serialize(),
+            dataType: 'json',
+            error: err => {
+                console.log(err);
+                el.text("An error occurred").addClass("alert-danger");
+                _this.prepend(el);
+                el.show('slow');
                 end_loader();
             },
-            success:function(resp){
+            success: function(resp){
                 end_loader();
-                if(resp.status == 'success'){
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Login Successful',
-                        text: 'You will be redirected shortly.',
-                        showConfirmButton: false,
-                        timer: 800
-                    }).then(() => {
-                        let redirect = new URLSearchParams(window.location.search).get('redirect');
-                        if(redirect == 'download') {
-                            let file_type = new URLSearchParams(window.location.search).get('file_type');
-                            let archive_id = new URLSearchParams(window.location.search).get('id');
-                            let download_url = '';
-                            if(file_type == 'zip') {
-                                download_url = '<?= base_url ?>uploads/files/Files-' + archive_id + '.zip';
-                            } else if(file_type == 'sql') {
-                                download_url = '<?= base_url ?>uploads/sql/SQL-' + archive_id + '.zip';
-                            } else if(file_type == 'pdf') {
-                                download_url = '<?= base_url ?>uploads/pdf/Document-' + archive_id + '.zip';
-                            }
-                            if(download_url) {
-                                window.location.href = download_url;
-                            }
-                        } else {
-                            location.href= "./";
+                if (resp.status == 'success') {
+                    // Set session privileges for download after login
+                    <?php $_SESSION['user_logged_in'] = true; ?>
+                    <?php $_SESSION['can_download'] = true; ?>
+
+                    // Redirect to the download URL if provided
+                    let redirect = new URLSearchParams(window.location.search).get('redirect');
+                    if(redirect == 'download') {
+                        let file_type = new URLSearchParams(window.location.search).get('file_type');
+                        let archive_id = new URLSearchParams(window.location.search).get('id');
+                        let download_url = '';
+                        if(file_type == 'zip') {
+                            download_url = '<?= base_url ?>uploads/files/Files-' + archive_id + '.zip';
+                        } else if(file_type == 'sql') {
+                            download_url = '<?= base_url ?>uploads/sql/SQL-' + archive_id + '.zip';
+                        } else if(file_type == 'pdf') {
+                            download_url = '<?= base_url ?>uploads/pdf/Document-' + archive_id + '.zip';
                         }
-                    })
-                }else if(!!resp.msg){
-                    el.text(resp.msg)
-                    el.addClass("alert-danger")
-                    _this.prepend(el)
-                    el.show('show')
-                }else{
-                    el.text("An error occurred while saving the data")
-                    el.addClass("alert-danger")
-                    _this.prepend(el)
-                    el.show('show')
+                        if(download_url) {
+                            window.location.href = download_url;
+                        }
+                    } else {
+                        location.href = "./";
+                    }
+                } else {
+                    el.text(resp.msg || "Login failed").addClass("alert-danger");
+                    _this.prepend(el);
+                    el.show('show');
                 }
-                end_loader();
-                $('html, body').animate({scrollTop: 0},'fast')
             }
-        })
-    })
-  })
+        });
+    });
+  });
 </script>
-
-
+</body>
+</html>
