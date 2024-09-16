0c5c83af39582d5b78de6f84ae7f5b70954a6feb
sherwintayo <sherwintayo08@gmail.com>
2024-09-16 23:07:41 +0800

    .

--- view_archive.php
+++ view_archive.php
@@ -48,6 +48,30 @@ if(isset($_GET['id']) && $_GET['id'] > 0){
                 <div class="container-fluid">
                     <h2><b><?= isset($title) ? htmlspecialchars($title) : "" ?></b></h2>
                     <small class="text-muted">Submitted by <b class="text-info"><?= htmlspecialchars($submitted) ?></b> on <?= isset($date_created) ? date("F d, Y h:i A", strtotime($date_created)) : "" ?></small>
+                    <?php if(isset($student_id) && $_settings->userdata('login_type') == "2" && $student_id == $_settings->userdata('id')): ?>
+                        <div class="form-group">
+                            <a href="./?page=submit-archive&id=<?= isset($id) ? htmlspecialchars($id) : "" ?>" class="btn btn-flat btn-default bg-navy btn-sm"><i class="fa fa-edit"></i> Edit</a>
+                            <button type="button" data-id="<?= isset($id) ? htmlspecialchars($id) : "" ?>" class="btn btn-flat btn-danger btn-sm delete-data"><i class="fa fa-trash"></i> Delete</button>
+                        </div>
+                    <?php endif; ?>
+                    <hr>
+                    <center>
+                        <img src="<?= validate_image(isset($banner_path) ? htmlspecialchars($banner_path) : "") ?>" alt="Banner Image" id="banner-img" class="img-fluid border bg-gradient-dark">
+                    </center>
+                    <fieldset>
+                        <legend class="text-navy">Project Year:</legend>
+                        <div class="pl-4"><large><?= isset($year) ? htmlspecialchars($year) : "----" ?></large></div>
+                    </fieldset>
+                    <fieldset>
+                        <legend class="text-navy">Abstract:</legend>
+                        <div class="pl-4">
+                            <large><?= isset($abstract) ? html_entity_decode($abstract) : "" ?></large>
+                        </div>
+                    </fieldset>
+                    <fieldset>
+                        <legend class="text-navy">Members:</legend>
+                        <div class="pl-4"><large><?= isset($members) ? html_entity_decode($members) : "" ?></large></div>
+                    </fieldset>
                     
                     <!-- Check if user is logged in and has download privilege -->
                     <?php if(isset($_SESSION['user_logged_in']) && $_SESSION['can_download'] == true): ?>
@@ -84,7 +108,6 @@ if(isset($_GET['id']) && $_GET['id'] > 0){
         </div>
     </div>
 </div>
-
 <script>
     $(function(){
         $('.delete-data').click(function(){
@@ -99,7 +122,7 @@ if(isset($_GET['id']) && $_GET['id'] > 0){
     function delete_archive(){
         start_loader();
         $.ajax({
-            url: _base_url_ + "classes/Master.php?f=delete_archive",
+            url: _base_url_ + "classes/Master.php?f=delete_archive",    
             method: "POST",
             data: {id: "<?= isset($id) ? htmlspecialchars($id) : "" ?>"},
             dataType: "json",
