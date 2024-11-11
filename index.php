8cf66993e73b626641c6c6f61bd820f5d5c68895
sherwintayo <sherwintayo08@gmail.com>
2024-11-11 20:38:57 +0800

    .

--- index.php
+++ index.php
@@ -89,7 +89,7 @@
         </div>
       <?php endif; ?>
       <!-- Main content -->
-      <section class="content" style="margin-top: 10vh;">
+      <section class="content" style="margin-top: 12vh;">
         <div class="container">
           <?php
           if (!file_exists($page . ".php") && !is_dir($page)) {
