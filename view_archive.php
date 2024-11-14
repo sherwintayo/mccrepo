3d7155add5c5642f512475dd41298f39dbc924b6
sherwintayo <sherwintayo08@gmail.com>
2024-11-14 17:13:36 +0800

    .

--- view_archive.php
+++ view_archive.php
@@ -1,12 +1,13 @@
 <?php
-session_start();
 
-// Check if user is logged in
-$is_logged_in = isset($_SESSION['user_logged_in']) && $_SESSION['user_logged_in'] === true;
+session_start(); // Start session
+
+// Check if user has download privileges (logged in and can download)
+$user_logged_in = isset($_SESSION['user_logged_in']) && $_SESSION['user_logged_in'] == true;
+$can_download = isset($_SESSION['can_download']) && $_SESSION['can_download'] == true;
 
 if (isset($_GET['id']) && $_GET['id'] > 0) {
-    // Fetch archive details from the database
-    $stmt = $conn->prepare("SELECT a.* FROM archive_list a WHERE a.id = ?");
+    $stmt = $conn->prepare("SELECT a.* FROM `archive_list` a WHERE a.id = ?");
     $stmt->bind_param("i", $_GET['id']);
     $stmt->execute();
     $qry = $stmt->get_result();
@@ -23,9 +24,10 @@ if (isset($_GET['id']) && $_GET['id'] > 0) {
         $stmt->bind_param("i", $student_id);
         $stmt->execute();
         $student = $stmt->get_result();
+
         if ($student->num_rows > 0) {
             $res = $student->fetch_array();
-            $submitted = $res['lastname'];
+            $submitted = $res['email'];
         }
     }
 
@@ -37,24 +39,8 @@ if (isset($_GET['id']) && $_GET['id'] > 0) {
 ?>
 <style>
     #document_field {
-        min-height: 80vh;
+        min-height: 80vh
     }
-
-    .download-info {
-        border: 1px solid #ccc;
-        padding: 10px;
-        border-radius: 5px;
-        margin-top: 10px;
-    }
-
-    #reasonTextarea,
-    #submitReasonButton {
-        display: none;
-    }
-
-    /* Hide textarea and submit button initially */
-</style>
-</head>
 </style>
 <div class="content py-4">
     <div class="col-12">
@@ -92,114 +78,99 @@ if (isset($_GET['id']) && $_GET['id'] > 0) {
                     <fieldset>
                         <legend class="text-navy">Abstract:</legend>
                         <div class="pl-4">
-                            <large><?= isset($abstract) ? htmlspecialchars($abstract) : "" ?></large>
+                            <large><?= isset($abstract) ? html_entity_decode($abstract) : "" ?></large>
                         </div>
                     </fieldset>
                     <fieldset>
                         <legend class="text-navy">Members:</legend>
                         <div class="pl-4">
-                            <large><?= isset($members) ? htmlspecialchars($members) : "" ?></large>
+                            <large><?= isset($members) ? html_entity_decode($members) : "" ?></large>
                         </div>
                     </fieldset>
+                    <!-- Project Files -->
+                    <fieldset>
+                        <legend class="text-navy">Project Files:</legend>
+                        <?php if ($user_logged_in && $can_download): ?>
+                            <a class="btn btn-success"
+                                href="<?= base_url . 'uploads/files/Files-' . htmlspecialchars($id) . '.zip' ?>">Download
+                                Project files</a>
+                        <?php else: ?>
+                            <a class="btn btn-success"
+                                href="login.php?redirect=download&file_type=zip&id=<?= htmlspecialchars($id) ?>">Download
+                                Project files</a>
+                        <?php endif; ?>
+                    </fieldset>
 
+                    <!-- SQL File -->
+                    <fieldset>
+                        <legend class="text-navy">SQL file:</legend>
+                        <?php if ($user_logged_in && $can_download): ?>
+                            <a class="btn btn-success"
+                                href="<?= base_url . 'uploads/sql/SQL-' . htmlspecialchars($id) . '.zip' ?>">Download SQL
+                                file</a>
+                        <?php else: ?>
+                            <a class="btn btn-success"
+                                href="login.php?redirect=download&file_type=sql&id=<?= htmlspecialchars($id) ?>">Download
+                                SQL file</a>
+                        <?php endif; ?>
+                    </fieldset>
 
-                    <!-- Single "Download All Files" Button with Description -->
-                    <div style="display: flex; align-items: center; margin-top: 20px;">
-                        <h5 class="text-navy" style="flex: 1;">Download all Files</h5>
-                        <button class="btn btn-success btn-flat" id="downloadButton">
-                            <i class="fa fa-download"></i> Download All Files
-                        </button>
-                    </div>
-
-                    <!-- File Information -->
-                    <div class="download-info">
-                        <p><strong>Project File:</strong>
-                            <?= isset($folder_path) ? basename($folder_path) : "Not available" ?></p>
-                        <p><strong>SQL File:</strong> <?= isset($sql_path) ? basename($sql_path) : "Not available" ?>
-                        </p>
-                        <p><strong>Document File:</strong>
-                            <?= isset($document_path) ? basename($document_path) : "Not available" ?></p>
-                    </div>
+                    <!-- Project Document -->
+                    <fieldset>
+                        <legend class="text-navy">Project Document:</legend>
+                        <?php if ($user_logged_in && $can_download): ?>
+                            <a class="btn btn-success"
+                                href="<?= base_url . 'uploads/pdf/Document-' . htmlspecialchars($id) . '.zip' ?>">Download
+                                Project Document</a>
+                        <?php else: ?>
+                            <a class="btn btn-success"
+                                href="login.php?redirect=download&file_type=pdf&id=<?= htmlspecialchars($id) ?>">Download
+                                Project Document</a>
+                        <?php endif; ?>
+                    </fieldset>
                 </div>
             </div>
         </div>
     </div>
-    <script>
-        $(function () {
-            $('.delete-data').click(function () {
-                _conf("Are you sure to delete <b>Archive-<?= isset($archive_code) ? htmlspecialchars($archive_code) : "" ?></b>", "delete_archive")
-            });
-            $('.summernote').summernote({
-                height: 200
-            });
-            $("#summernote").summernote("disable");
+</div>
+<script>
+    $(function () {
+        $('.delete-data').click(function () {
+            _conf("Are you sure to delete <b>Archive-<?= isset($archive_code) ? htmlspecialchars($archive_code) : "" ?></b>", "delete_archive")
         });
+        $('.summernote').summernote({
+            height: 200
+        });
+        $("#summernote").summernote("disable");
+    });
 
-        function delete_archive() {
-            start_loader();
-            $.ajax({
-                url: _base_url_ + "classes/Master.php?f=delete_archive",
-                method: "POST",
-                data: { id: "<?= isset($id) ? htmlspecialchars($id) : "" ?>" },
-                dataType: "json",
-                error: err => {
-                    console.log(err);
+    function delete_archive() {
+        start_loader();
+        $.ajax({
+            url: _base_url_ + "classes/Master.php?f=delete_archive",
+            method: "POST",
+            data: { id: "<?= isset($id) ? htmlspecialchars($id) : "" ?>" },
+            dataType: "json",
+            error: err => {
+                console.log(err);
+                alert_toast("An error occurred.", 'error');
+                end_loader();
+            },
+            success: function (resp) {
+                if (typeof resp == 'object' && resp.status == 'success') {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Deleted Successfully',
+                        showConfirmButton: false,
+                        timer: 1000
+                    }).then(() => {
+                        location.replace("./");
+                    });
+                } else {
                     alert_toast("An error occurred.", 'error');
                     end_loader();
-                },
-                success: function (resp) {
-                    if (typeof resp == 'object' && resp.status == 'success') {
-                        Swal.fire({
-                            icon: 'success',
-                            title: 'Deleted Successfully',
-                            showConfirmButton: false,
-                            timer: 1000
-                        }).then(() => {
-                            location.replace("./");
-                        });
-                    } else {
-                        alert_toast("An error occurred.", 'error');
-                        end_loader();
-                    }
                 }
-            });
-        }
-
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
-
-
-    <?php
-    // Assign the PHP paths to JavaScript variables
-    $document_path = 'uploads/pdf/Document-' . $aid . '.zip';
-    $project_path = 'uploads/files/Files-' . $aid . '.zip';
-    $sql_path = 'uploads/sql/SQL-' . $aid . '.zip';
-    ?>
-    <script>
-        $(document).ready(function () {
-            // Handle download button click
-            $('#downloadButton').click(function () {
-                // Create an invisible link to download all files
-                const zip = new JSZip();
-
-                // Assign PHP values to JavaScript
-                <?php if (isset($document_path)): ?>
-                    zip.file("Document File.zip", "<?= htmlspecialchars($document_path) ?>");
-                <?php endif; ?>
-                <?php if (isset($project_path)): ?>
-                    zip.file("Project File.zip", "<?= htmlspecialchars($project_path) ?>");
-                <?php endif; ?>
-                <?php if (isset($sql_path)): ?>
-                    zip.file("SQL File.zip", "<?= htmlspecialchars($sql_path) ?>");
-                <?php endif; ?>
-
-                zip.generateAsync({ type: "blob" }).then(function (content) {
-                    const link = document.createElement('a');
-                    link.href = URL.createObjectURL(content);
-                    link.download = "All_Files.zip"; // Name of the zip file
-                    link.click();
-                });
-            });
+            }
         });
-    </script>
+    }
+</script>
