793d67b7aaf61a1cebe6a9452f7389e5b5596ce6
sherwintayo <sherwintayo08@gmail.com>
2024-11-19 18:40:08 +0800

    system info and back comt

--- admin/system_info/index.php
+++ admin/system_info/index.php
@@ -1,23 +1,24 @@
-<?php if($_settings->chk_flashdata('success')): ?>
+<?php if ($_settings->chk_flashdata('success')): ?>
 	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script>
-	
-    Swal.fire({
-        title: 'Success!',
-        text: '<?php echo $_settings->flashdata('success') ?>',
-        icon: 'success'
-    });
-</script>
+	<script>
+
+		Swal.fire({
+			title: 'Success!',
+			text: '<?php echo $_settings->flashdata('success') ?>',
+			icon: 'success'
+		});
+	</script>
 <?php endif; ?>
 
 <style>
-	img#cimg{
+	img#cimg {
 		height: 15vh;
 		width: 15vh;
 		object-fit: scale-down;
 		border-radius: 100% 100%;
 	}
-	img#cimg2{
+
+	img#cimg2 {
 		height: 50vh;
 		width: 100%;
 		object-fit: contain;
@@ -34,58 +35,69 @@
 		</div>
 		<div class="card-body">
 			<form action="" id="system-frm">
-			<div id="msg" class="form-group"></div>
-			<div class="form-group">
-				<label for="name" class="control-label">System Name</label>
-				<input type="text" class="form-control form-control-sm" name="name" id="name" value="<?php echo $_settings->info('name') ?>">
-			</div>
-			<div class="form-group">
-				<label for="short_name" class="control-label">System Short Name</label>
-				<input type="text" class="form-control form-control-sm" name="short_name" id="short_name" value="<?php echo  $_settings->info('short_name') ?>">
-			</div>
-			<div class="form-group">
-				<label for="content[about_us]" class="control-label">Welcome Content</label>
-				<textarea type="text" class="form-control form-control-sm summernote" name="content[welcome]" id="welcome"><?php echo  is_file(base_app.'welcome.html') ? file_get_contents(base_app.'welcome.html') : '' ?></textarea>
-			</div>
-			<div class="form-group">
-				<label for="content[about_us]" class="control-label">About Us</label>
-				<textarea type="text" class="form-control form-control-sm summernote" name="content[about_us]" id="about_us"><?php echo  is_file(base_app.'about_us.html') ? file_get_contents(base_app.'about_us.html') : '' ?></textarea>
-			</div>
-			<div class="form-group">
-				<label for="" class="control-label">System Logo</label>
-				<div class="custom-file">
-	              <input type="file" class="custom-file-input rounded-circle" id="customFile" name="img" onchange="displayImg(this,$(this))">
-	              <label class="custom-file-label" for="customFile">Choose file</label>
-	            </div>
-			</div>
-			<div class="form-group d-flex justify-content-center">
-				<img src="<?php echo validate_image($_settings->info('logo')) ?>" alt="" id="cimg" class="img-fluid img-thumbnail">
-			</div>
-			<div class="form-group">
-				<label for="" class="control-label">Cover</label>
-				<div class="custom-file">
-	              <input type="file" class="custom-file-input rounded-circle" id="customFile" name="cover" onchange="displayImg2(this,$(this))">
-	              <label class="custom-file-label" for="customFile">Choose file</label>
-	            </div>
-			</div>
-			<div class="form-group d-flex justify-content-center">
-				<img src="<?php echo validate_image($_settings->info('cover')) ?>" alt="" id="cimg2" class="img-fluid img-thumbnail bg-gradient-dark border-dark">
-			</div>
-			<fieldset>
-				<legend>School Information</legend>
+				<div id="msg" class="form-group"></div>
 				<div class="form-group">
-					<label for="email" class="control-label">Email</label>
-					<input type="email" class="form-control form-control-sm" name="email" id="email" value="<?php echo $_settings->info('email') ?>">
+					<label for="name" class="control-label">System Name</label>
+					<input type="text" class="form-control form-control-sm" name="name" id="name"
+						value="<?php echo $_settings->info('name') ?>">
 				</div>
 				<div class="form-group">
-					<label for="contact" class="control-label">Contact #</label>
-					<input type="text" class="form-control form-control-sm" name="contact" id="contact" value="<?php echo $_settings->info('contact') ?>">
+					<label for="short_name" class="control-label">System Short Name</label>
+					<input type="text" class="form-control form-control-sm" name="short_name" id="short_name"
+						value="<?php echo $_settings->info('short_name') ?>">
 				</div>
 				<div class="form-group">
-					<label for="address" class="control-label">Address</label>
-					<textarea rows="3" class="form-control form-control-sm" name="address" id="address" style="resize:none"><?php echo $_settings->info('address') ?></textarea>
+					<label for="content[about_us]" class="control-label">Welcome Content</label>
+					<textarea type="text" class="form-control form-control-sm summernote" name="content[welcome]"
+						id="welcome"><?php echo is_file(base_app . 'welcome.html') ? file_get_contents(base_app . 'welcome.html') : '' ?></textarea>
 				</div>
-			</fieldset>
+				<div class="form-group">
+					<label for="content[about_us]" class="control-label">About Us</label>
+					<textarea type="text" class="form-control form-control-sm summernote" name="content[about_us]"
+						id="about_us"><?php echo is_file(base_app . 'about_us.html') ? file_get_contents(base_app . 'about_us.html') : '' ?></textarea>
+				</div>
+				<div class="form-group">
+					<label for="" class="control-label">System Logo</label>
+					<div class="custom-file">
+						<input type="file" class="custom-file-input rounded-circle" id="customFile" name="img"
+							onchange="displayImg(this,$(this))">
+						<label class="custom-file-label" for="customFile">Choose file</label>
+					</div>
+				</div>
+				<div class="form-group d-flex justify-content-center">
+					<img src="<?php echo validate_image($_settings->info('logo')) ?>" alt="" id="cimg"
+						class="img-fluid img-thumbnail">
+				</div>
+				<div class="form-group">
+					<label for="" class="control-label">Cover</label>
+					<div class="custom-file">
+						<input type="file" class="custom-file-input rounded-circle" id="customFile" name="cover"
+							onchange="displayImg2(this,$(this))">
+						<label class="custom-file-label" for="customFile">Choose file</label>
+					</div>
+				</div>
+				<div class="form-group d-flex justify-content-center">
+					<img src="<?php echo validate_image($_settings->info('cover')) ?>" alt="" id="cimg2"
+						class="img-fluid img-thumbnail bg-gradient-dark border-dark">
+				</div>
+				<fieldset>
+					<legend>School Information</legend>
+					<div class="form-group">
+						<label for="email" class="control-label">Email</label>
+						<input type="email" class="form-control form-control-sm" name="email" id="email"
+							value="<?php echo $_settings->info('email') ?>">
+					</div>
+					<div class="form-group">
+						<label for="contact" class="control-label">Contact #</label>
+						<input type="text" class="form-control form-control-sm" name="contact" id="contact"
+							value="<?php echo $_settings->info('contact') ?>">
+					</div>
+					<div class="form-group">
+						<label for="address" class="control-label">Address</label>
+						<textarea rows="3" class="form-control form-control-sm" name="address" id="address"
+							style="resize:none"><?php echo $_settings->info('address') ?></textarea>
+					</div>
+				</fieldset>
 			</form>
 		</div>
 		<div class="card-footer">
@@ -99,53 +111,53 @@
 	</div>
 </div>
 <script>
-	function displayImg(input,_this) {
-	    if (input.files && input.files[0]) {
-	        var reader = new FileReader();
-	        reader.onload = function (e) {
-	        	$('#cimg').attr('src', e.target.result);
-	        	_this.siblings('.custom-file-label').html(input.files[0].name)
-	        }
+	function displayImg(input, _this) {
+		if (input.files && input.files[0]) {
+			var reader = new FileReader();
+			reader.onload = function (e) {
+				$('#cimg').attr('src', e.target.result);
+				_this.siblings('.custom-file-label').html(input.files[0].name)
+			}
 
-	        reader.readAsDataURL(input.files[0]);
-	    }
+			reader.readAsDataURL(input.files[0]);
+		}
 	}
-	function displayImg2(input,_this) {
-	    if (input.files && input.files[0]) {
-	        var reader = new FileReader();
-	        reader.onload = function (e) {
-	        	_this.siblings('.custom-file-label').html(input.files[0].name)
-	        	$('#cimg2').attr('src', e.target.result);
-	        }
+	function displayImg2(input, _this) {
+		if (input.files && input.files[0]) {
+			var reader = new FileReader();
+			reader.onload = function (e) {
+				_this.siblings('.custom-file-label').html(input.files[0].name)
+				$('#cimg2').attr('src', e.target.result);
+			}
 
-	        reader.readAsDataURL(input.files[0]);
-	    }
+			reader.readAsDataURL(input.files[0]);
+		}
 	}
-	function displayImg3(input,_this) {
-	    if (input.files && input.files[0]) {
-	        var reader = new FileReader();
-	        reader.onload = function (e) {
-	        	_this.siblings('.custom-file-label').html(input.files[0].name)
-	        	$('#cimg3').attr('src', e.target.result);
-	        }
+	function displayImg3(input, _this) {
+		if (input.files && input.files[0]) {
+			var reader = new FileReader();
+			reader.onload = function (e) {
+				_this.siblings('.custom-file-label').html(input.files[0].name)
+				$('#cimg3').attr('src', e.target.result);
+			}
 
-	        reader.readAsDataURL(input.files[0]);
-	    }
+			reader.readAsDataURL(input.files[0]);
+		}
 	}
-	$(document).ready(function(){
-		 $('.summernote').summernote({
-		        height: 200,
-		        toolbar: [
-		            [ 'style', [ 'style' ] ],
-		            [ 'font', [ 'bold', 'italic', 'underline', 'strikethrough', 'superscript', 'subscript', 'clear'] ],
-		            [ 'fontname', [ 'fontname' ] ],
-		            [ 'fontsize', [ 'fontsize' ] ],
-		            [ 'color', [ 'color' ] ],
-		            [ 'para', [ 'ol', 'ul', 'paragraph', 'height' ] ],
-		            [ 'table', [ 'table' ] ],
-					['insert', ['link', 'picture']],
-		            [ 'view', [ 'undo', 'redo', 'fullscreen', 'codeview', 'help' ] ]
-		        ]
-		    })
+	$(document).ready(function () {
+		$('.summernote').summernote({
+			height: 200,
+			toolbar: [
+				['style', ['style']],
+				['font', ['bold', 'italic', 'underline', 'strikethrough', 'superscript', 'subscript', 'clear']],
+				['fontname', ['fontname']],
+				['fontsize', ['fontsize']],
+				['color', ['color']],
+				['para', ['ol', 'ul', 'paragraph', 'height']],
+				['table', ['table']],
+				['insert', ['link', 'picture']],
+				['view', ['undo', 'redo', 'fullscreen', 'codeview', 'help']]
+			]
+		})
 	})
 </script>
