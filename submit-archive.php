0a89ed935edaddbf5f0031bd34c4d6c3a2b25f7e
sherwintayo <sherwintayo08@gmail.com>
2024-11-24 03:19:18 +0800

    .

--- submit-archive.php
+++ submit-archive.php
@@ -15,19 +15,6 @@ if (isset($_GET['id']) && $_GET['id'] > 0) {
 }
 ?>
 <style>
-    .modal {
-        position: fixed;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        background-color: white;
-        z-index: 1050;
-        padding: 20px;
-        border-radius: 8px;
-        box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.3);
-        min-width: 300px;
-    }
-
     .progress {
         background-color: #f0f0f0;
         height: 8px;
