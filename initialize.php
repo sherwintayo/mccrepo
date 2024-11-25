969f977d1e1c1f67112a6b49bb657bd0a440183d
sherwintayo <sherwintayo08@gmail.com>
2024-11-25 17:03:15 +0800

    .

--- initialize.php
+++ initialize.php
@@ -1,28 +1,22 @@
 <?php
 
-// if (!defined('base_url'))
-//     define('base_url', 'https://mccbsitrepositories.com/');
-// if (!defined('base_app'))
-//     define('base_app', str_replace('\\', '/', __DIR__) . '/');
-// if (!defined('DB_SERVER'))
-//     define('DB_SERVER', "localhost");
-// if (!defined('DB_USERNAME'))
-//     define('DB_USERNAME', "u510162695_bsit_repo");
-// if (!defined('DB_PASSWORD'))
-//     define('DB_PASSWORD', "1Bsit_repo");
-// if (!defined('DB_NAME'))
-//     define('DB_NAME', "u510162695_bsit_repo");
-
 if (!defined('base_url'))
-    define('base_url', 'http://localhost/mccrepo/');
+    define('base_url', 'https://mccbsitrepositories.com/');
 if (!defined('base_app'))
     define('base_app', str_replace('\\', '/', __DIR__) . '/');
 if (!defined('DB_SERVER'))
     define('DB_SERVER', "localhost");
 if (!defined('DB_USERNAME'))
-    define('DB_USERNAME', "root");
+    define('DB_USERNAME', "u510162695_bsit_repo");
 if (!defined('DB_PASSWORD'))
-    define('DB_PASSWORD', "");
+    define('DB_PASSWORD', "1Bsit_repo");
 if (!defined('DB_NAME'))
-    define('DB_NAME', "bsit_projectrepo_db");
+    define('DB_NAME', "u510162695_bsit_repo");
+
+// if(!defined('base_url')) define('base_url','http://localhost/mccrepo/');
+//     if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
+//     if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
+//     if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
+//     if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
+//     if(!defined('DB_NAME')) define('DB_NAME',"bsit_projectrepo_db");
 ?>
