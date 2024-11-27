fe13978daffb61a7a095f0f66bae32fe22cca736
sherwintayo <sherwintayo08@gmail.com>
2024-11-27 17:10:50 +0800

    .

--- myScripts/passvalidation.js
+++ myScripts/passvalidation.js
@@ -10,8 +10,8 @@ const requirements = [
   { regex: /.{8,}/, index: 0 }, // Minimum of 8 characters
   { regex: /[0-9]/, index: 1 }, // At least one number
   { regex: /[a-z]/, index: 2 }, // At least one lowercase letter
-  // { regex: /[^A-Za-z0-9]/, index: 3 }, // At least one special character
   { regex: /[A-Z]/, index: 3 }, // At least one uppercase letter
+  // { regex: /[^A-Za-z0-9]/, index: 3 }, // At least one special character
 ];
 
 passwordInput.addEventListener("keyup", (e) => {
