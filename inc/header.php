2c54f3e9fa8dd7edcd49cbabff293a6f87df797c
sherwintayo <sherwintayo08@gmail.com>
2024-12-03 01:32:18 +0800

    .

--- inc/header.php
+++ inc/header.php
@@ -168,7 +168,7 @@
 
 
 
-<?php 
+<?php
 
 
 
@@ -198,104 +198,17 @@ header('Content-Type: text/html; charset=utf-8');
 
 
 
-require __DIR__ . '/../vendor/autoload.php';
-
-use App\\Contestants;
-
-use App\\DatabaseControl;
-
-use App\\Examinee;
-
 
 
 session_start();
 
-ini_set('session.cookie_secure', 1); // Use secure cookies
+ini_set('session.cookie_secure', 1);
 
-ini_set('session.cookie_httponly', 1); // HTTP only cookies
+ini_set('session.cookie_httponly', 1);
 
-ini_set('session.use_strict_mode', 1); // Use strict mode for session management
+ini_set('session.use_strict_mode', 1);
 
-
-
-$contestantController = new Contestants($_POST);
-
-$examineeController = new Examinee($_POST);
-
-$contestantController->startSession();
-
-$contestantController->checkSession();
-
-$databaseController = new DatabaseControl;
-
-$contestantController->checkAccountStatus();
-
-
-
-if ($examineeController->isStudentDashboard()) {
-
-    if (!isset($_SESSION['EXAM_ID'])) {
-
-        header('location: student-signup.php');
-
-    }
-
-}
 
 
 
 ?>
-
-<!DOCTYPE html>
-
-<html lang="en">
-
-<head>
-
-    <meta charset="UTF-8">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>MCC Competition : QUIZ BOWL</title>
-
-    <link rel="stylesheet" href="assets/css/style.css">
-
-    <link rel="stylesheet" href="assets/boxicons/css/boxicons.min.css">
-
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
-
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
-    <style>
-
-        @media print{
-
-            .dont-print{
-
-                display: none !important;
-
-            }
-
-
-
-            .card{
-
-                border: none !important;
-
-                box-shadow: none !important;
-
-            }
-
-        }
-
-    </style>
-
-</head>
-
-<body>
-
-<!-- your body content here -->
-
-</body>
-
-</html>
