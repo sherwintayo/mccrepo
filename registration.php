7885d40722d46bfd415c033e23d4d4a7b9d21364
sherwintayo <sherwintayo08@gmail.com>
2024-12-05 00:50:41 +0800

    .

--- registration.php
+++ registration.php
@@ -270,7 +270,7 @@ $stmt->fetch();
             these conditions.</p>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary" data-dismiss="modal">OK</button>
         </div>
       </div>
     </div>
