cf2d6c440e67ac08594a2448bc7e10f5e4be1f7f
sherwintayo <sherwintayo08@gmail.com>
2024-12-07 00:03:01 +0800

    .

--- projects.php
+++ projects.php
@@ -32,7 +32,7 @@
             while ($row = $archives->fetch_assoc()) {
                 $row['abstract'] = strip_tags(html_entity_decode($row['abstract']));
                 ?>
-                <div class="card col-lg-3 col-md-4 mb-2">
+                <div class="cards col-lg-3 col-md-4 mb-2">
                     <a href="./?page=view_archive&id=<?= $row['id'] ?>"
                         class="rounded-0 shadow book-item text-decoration-none">
                         <div class="img-holder overflow-hidden">
