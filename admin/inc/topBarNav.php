32b7b143a4b37aa4b4633a53ff99091e1b2d3506
sherwintayo <sherwintayo08@gmail.com>
2024-12-12 03:11:17 +0800

    .

--- admin/inc/topBarNav.php
+++ admin/inc/topBarNav.php
@@ -210,6 +210,28 @@ while ($row = $result->fetch_assoc()) {
         </a>
       </div>
     </li>
+
+    <!-- User Dropdown Menu -->
+    <li class="nav-item">
+      <div class="btn-group nav-link">
+        <button type="button" class="btn btn-rounded badge badge-light dropdown-toggle dropdown-icon"
+          data-toggle="dropdown">
+          <span><img src="<?php echo validate_image($_settings->userdata('avatar')) ?>"
+              class="img-circle elevation-2 user-img" alt="User Image"></span>
+          <span
+            class="ml-3"><?php echo ucwords($_settings->userdata('firstname') . ' ' . $_settings->userdata('lastname')) ?></span>
+          <span class="sr-only">Toggle Dropdown</span>
+        </button>
+        <div class="dropdown-menu" role="menu">
+          <a class="dropdown-item" href="<?php echo base_url . 'admin/?page=user' ?>"><span class="fa fa-user"></span>
+            My
+            Account</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="<?php echo base_url . '/classes/Login.php?f=logout' ?>"><span
+              class="fas fa-sign-out-alt"></span> Logout</a>
+        </div>
+      </div>
+    </li>
   </ul>
 </nav>
 <!-- /.navbar -->
