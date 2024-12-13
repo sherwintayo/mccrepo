3bed0e83876657e7df3f37f6cf64ec09912dce1e
sherwintayo <sherwintayo08@gmail.com>
2024-12-14 04:52:33 +0800

    .

--- projects_per_program.php
+++ projects_per_program.php
@@ -1,6 +1,5 @@
 <?php
 if (isset($_GET['id'])) {
-
     $qry = $conn->query("SELECT * FROM program_list where `status` = 1 and id = '{$_GET['id']}' ");
     if ($qry->num_rows > 0) {
         foreach ($qry->fetch_assoc() as $k => $v) {
@@ -9,78 +8,72 @@ if (isset($_GET['id'])) {
             }
         }
     } else {
-        echo "<script> alert('Unkown Program ID'); location.replace('./') </script>";
+        echo "<script> alert('Unknown Program ID'); location.replace('./') </script>";
     }
-
 } else {
     echo "<script> alert('Program ID is required'); location.replace('./') </script>";
 }
-
 ?>
-<div class="content py-2">
-    <div class="col-12">
-        <div class="card card-outline card-primary shadow rounded-0">
-            <div class="card-body rounded-0">
-                <h2>Archive List of <?= isset($program['name']) ? $program['name'] : "" ?> </h2>
-                <p><small><?= isset($program['description']) ? $program['description'] : "" ?></small></p>
-                <hr class="bg-navy">
-                <?php
-                $id = isset($_GET['id']) ? $_GET['id'] : '';
-                $limit = 10;
-                $page = isset($_GET['p']) ? $_GET['p'] : 1;
-                $offset = 10 * ($page - 1);
-                $paginate = " limit {$limit} offset {$offset}";
-                $wheredid = " where program_id = '{$id}' ";
-                $students = $conn->query("SELECT * FROM `student_list` where id in (SELECT student_id FROM archive_list where `status` = 1 and curriculum_id in (SELECT id from curriculum_list {$wheredid} ))");
-                $student_arr = array_column($students->fetch_all(MYSQLI_ASSOC), 'lastname', 'id');
-                $count_all = $conn->query("SELECT * FROM archive_list where `status` = 1 and curriculum_id in (SELECT id from curriculum_list {$wheredid} )")->num_rows;
-                $pages = ceil($count_all / $limit);
-                $archives = $conn->query("SELECT * FROM archive_list where `status` = 1 and curriculum_id in (SELECT id from curriculum_list {$wheredid} ) order by unix_timestamp(date_created) desc {$paginate}");
-                ?>
-                <div class="list-group">
-                    <?php
-                    while ($row = $archives->fetch_assoc()):
-                        $row['abstract'] = strip_tags(html_entity_decode($row['abstract']));
-                        ?>
-                        <a href="./?page=view_archive&id=<?= $row['id'] ?>"
-                            class="text-decoration-none text-dark list-group-item list-group-item-action">
-                            <div class="row">
-                                <div class="col-lg-4 col-md-5 col-sm-12 text-center">
-                                    <img src="<?= validate_image($row['banner_path']) ?>"
-                                        class="banner-img img-fluid bg-gradient-dark" alt="Banner Image">
-                                </div>
-                                <div class="col-lg-8 col-md-7 col-sm-12">
-                                    <h3 class="text-navy"><b><?php echo $row['title'] ?></b></h3>
-                                    <small class="text-muted">By <b
-                                            class="text-info"><?= isset($student_arr[$row['student_id']]) ? $student_arr[$row['student_id']] : "N/A" ?></b></small>
-                                    <p class="truncate-5"><?= $row['abstract'] ?></p>
-                                </div>
-                            </div>
-                        </a>
-                    <?php endwhile; ?>
-                </div>
-            </div>
-            <div class="card-footer clearfix rounded-0">
-                <div class="col-12">
-                    <div class="row">
-                        <div class="col-md-6"><span class="text-muted">Display Items: <?= $archives->num_rows ?></span>
-                        </div>
-                        <div class="col-md-6">
-                            <ul class="pagination pagination-sm m-0 float-right">
-                                <li class="page-item"><a class="page-link"
-                                        href="./?page=projects_per_program&id=<?= $id ?>&p=<?= $page - 1 ?>" <?= $page == 1 ? 'disabled' : '' ?>>«</a></li>
-                                <?php for ($i = 1; $i <= $pages; $i++): ?>
-                                    <li class="page-item"><a class="page-link <?= $page == $i ? 'active' : '' ?>"
-                                            href="./?page=projects_per_program&id=<?= $id ?>&p=<?= $i ?>"><?= $i ?></a></li>
-                                <?php endfor; ?>
-                                <li class="page-item"><a class="page-link"
-                                        href="./?page=projects_per_program&id=<?= $id ?>&p=<?= $page + 1 ?>"
-                                        <?= $page == $pages || $pages <= 1 ? 'disabled' : '' ?>>»</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
 
+<div class="content project-container py-2 h-100">
+    <div class="col-12">
+        <div class="head">
+            <h2 class="text-center">Projects for <?= isset($program['name']) ? $program['name'] : "" ?></h2>
+        </div>
+        <p class="text-center"><small><?= isset($program['description']) ? $program['description'] : "" ?></small></p>
+        <?php
+        $id = isset($_GET['id']) ? $_GET['id'] : '';
+        $limit = 10;
+        $page = isset($_GET['p']) ? $_GET['p'] : 1;
+        $offset = 10 * ($page - 1);
+        $paginate = " limit {$limit} offset {$offset}";
+        $wheredid = " where program_id = '{$id}' ";
+        $students = $conn->query("SELECT * FROM `student_list` where id in (SELECT student_id FROM archive_list where `status` = 1 and curriculum_id in (SELECT id from curriculum_list {$wheredid} ))");
+        $student_arr = array_column($students->fetch_all(MYSQLI_ASSOC), 'lastname', 'id');
+        $count_all = $conn->query("SELECT * FROM archive_list where `status` = 1 and curriculum_id in (SELECT id from curriculum_list {$wheredid} )")->num_rows;
+        $pages = ceil($count_all / $limit);
+        $archives = $conn->query("SELECT * FROM archive_list where `status` = 1 and curriculum_id in (SELECT id from curriculum_list {$wheredid} ) order by unix_timestamp(date_created) desc {$paginate}");
+        ?>
+        <div class="row">
+            <?php
+            while ($row = $archives->fetch_assoc()):
+                $row['abstract'] = strip_tags(html_entity_decode($row['abstract']));
+                ?>
+                <div class="cards col-lg-3 col-md-4 mb-2">
+                    <a href="./?page=view_archive&id=<?= $row['id'] ?>" class="shadow book-item text-decoration-none">
+                        <div class="img-holder banner overflow-hidden">
+                            <img class="img-top" src="<?= validate_image($row['banner_path']) ?>" alt="Banner Image">
+                        </div>
+                        <div class="cards-body">
+                            <div class="cards-title fw-bolder h5 text-center"><?= $row['title'] ?></div><br>
+                            <div class="student">
+                                <small>By:
+                                    <b><?= isset($student_arr[$row['student_id']]) ? $student_arr[$row['student_id']] : "N/A" ?></b>
+                                </small>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            <?php endwhile; ?>
+        </div>
+
+        <div class="card-footer clearfix rounded-0">
+            <div class="d-flex justify-content-center">
+                <ul class="pagination pagination-sm">
+                    <li class="page-item">
+                        <a class="page-link" href="./?page=projects_per_program&id=<?= $id ?>&p=<?= $page - 1 ?>"
+                            <?= $page == 1 ? 'disabled' : '' ?>>«</a>
+                    </li>
+                    <?php for ($i = 1; $i <= $pages; $i++): ?>
+                        <li class="page-item <?= $page == $i ? 'active' : '' ?>">
+                            <a class="page-link" href="./?page=projects_per_program&id=<?= $id ?>&p=<?= $i ?>"><?= $i ?></a>
+                        </li>
+                    <?php endfor; ?>
+                    <li class="page-item">
+                        <a class="page-link" href="./?page=projects_per_program&id=<?= $id ?>&p=<?= $page + 1 ?>"
+                            <?= $page == $pages ? 'disabled' : '' ?>>»</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
