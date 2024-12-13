27f5681580deb9c2d22d4e99145cd5ccb7bdd039
sherwintayo <sherwintayo08@gmail.com>
2024-12-13 13:13:42 +0800

    embed mccchat.ocom

--- index.php
+++ index.php
@@ -103,21 +103,10 @@
       </section>
 
       <div class="floating-buttons">
-        <?php if ($_settings->userdata('id') > 0): ?>
-          <!-- User Manual Button -->
-          <div class="floating-btn-container">
-            <button class="floating-btn user-manual-btn" onclick="toggleSidebar('userManualSidebar')">
-              <i class="fa fa-book"></i>
-            </button>
-            <span class="floating-btn-text">Click User Manual</span>
-          </div>
-        <?php endif; ?>
-        <!-- Chatbot Button -->
+
         <div class="floating-btn-container">
-          <button class="floating-btn chatbot-btn">
-            <a href="https://mccchat.com/" target="_blank">
-              <i class="fa fa-comments"></i>
-            </a>
+          <button class="floating-btn user-manual-btn" onclick="toggleSidebar('userManualSidebar')">
+            <i class="fa fa-book"></i>
           </button>
           <span class="floating-btn-text">Click Chatbot</span>
         </div>
@@ -128,75 +117,76 @@
       <div id="userManualSidebar" class="sidebar hidden">
         <a href="javascript:void(0)" class="closebtn" onclick="toggleSidebar('userManualSidebar')">&times;</a>
         <div class="container">
-          <h2>User Manual</h2>
+          <h2>Chatbot</h2>
           <div class="sidebar-content">
-            <p>This is the user manual content.</p>
+            <!-- Embed your mccchat.com site here -->
+            <iframe src="https://mccchat.com" width="100%" height="500px" style="border: none;">
+            </iframe>
           </div>
         </div>
-      </div>
-      <!-- Background Overlay -->
-      <div id="overlay" class="overlay hidden" onclick="closeAllSidebars()"></div>
+        <!-- Background Overlay -->
+        <div id="overlay" class="overlay hidden" onclick="closeAllSidebars()"></div>
 
 
 
-      <!-- /.content -->
-      <div class="modal fade" id="confirm_modal" role='dialog'>
-        <div class="modal-dialog modal-md modal-dialog-centered" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title">Confirmation</h5>
+        <!-- /.content -->
+        <div class="modal fade" id="confirm_modal" role='dialog'>
+          <div class="modal-dialog modal-md modal-dialog-centered" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title">Confirmation</h5>
+              </div>
+              <div class="modal-body">
+                <div id="delete_content"></div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-primary" id='confirm' onclick="">Continue</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              </div>
             </div>
-            <div class="modal-body">
-              <div id="delete_content"></div>
+          </div>
+        </div>
+        <div class="modal fade" id="uni_modal" role='dialog'>
+          <div class="modal-dialog modal-md modal-dialog-centered" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title"></h5>
+              </div>
+              <div class="modal-body">
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-primary" id='submit'
+                  onclick="$('#uni_modal form').submit()">Save</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+              </div>
             </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-primary" id='confirm' onclick="">Continue</button>
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+        <div class="modal fade" id="uni_modal_right" role='dialog'>
+          <div class="modal-dialog modal-full-height  modal-md" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span class="fa fa-arrow-right"></span>
+                </button>
+              </div>
+              <div class="modal-body">
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="modal fade" id="viewer_modal" role='dialog'>
+          <div class="modal-dialog modal-md" role="document">
+            <div class="modal-content">
+              <button type="button" class="btn-close" data-dismiss="modal"><span class="fa fa-times"></span></button>
+              <img src="" alt="">
             </div>
           </div>
         </div>
       </div>
-      <div class="modal fade" id="uni_modal" role='dialog'>
-        <div class="modal-dialog modal-md modal-dialog-centered" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title"></h5>
-            </div>
-            <div class="modal-body">
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-primary" id='submit'
-                onclick="$('#uni_modal form').submit()">Save</button>
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="modal fade" id="uni_modal_right" role='dialog'>
-        <div class="modal-dialog modal-full-height  modal-md" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title"></h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span class="fa fa-arrow-right"></span>
-              </button>
-            </div>
-            <div class="modal-body">
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="modal fade" id="viewer_modal" role='dialog'>
-        <div class="modal-dialog modal-md" role="document">
-          <div class="modal-content">
-            <button type="button" class="btn-close" data-dismiss="modal"><span class="fa fa-times"></span></button>
-            <img src="" alt="">
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- /content-wrapper -->
-    <?php require_once('inc/footer.php') ?>
+      <!-- /content-wrapper -->
+      <?php require_once('inc/footer.php') ?>
 </body>
 <script src="https://unpkg.com/@dotlottie/player-component@2.7.12/dist/dotlottie-player.mjs" type="module"></script>
 <script>
