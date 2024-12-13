0a7a1656ff4dc36077e567cbdd379d8b2589861e
sherwintayo <sherwintayo08@gmail.com>
2024-12-14 05:23:59 +0800

    .

--- admin/projects/index.php
+++ admin/projects/index.php
@@ -10,6 +10,7 @@
 <!-- Placed at the end of the document so the pages load faster -->
 <!-- <script type="text/javascript" src="../../bootstrap/js/jquery-3.6.0.min.js"></script>
     <script type="text/javascript" src="../../bootstrap/js/bootstrap.bundle.min.js"></script> -->
+<link rel="stylesheet" href="<?php echo base_url ?>myStyles/projects.css?v=<?php echo time(); ?>">
 <style>
     .img-top {
         border: 1px solid #ddd;
