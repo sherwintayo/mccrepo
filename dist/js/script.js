1e0f25d014f1a0f86d144dfc8c85df2f613ebf3a
sherwintayo <sherwintayo08@gmail.com>
2024-12-13 20:32:52 +0800

    .

--- dist/js/script.js
+++ dist/js/script.js
@@ -124,29 +124,29 @@ $(document).ready(function () {
     });
   });
   // System Info
-  $("#system-frm").submit(function (e) {
-    e.preventDefault();
-    start_loader();
-    if ($(".err_msg").length > 0) $(".err_msg").remove();
-    $.ajax({
-      url: _base_url_ + "classes/SystemSettings.php?f=update_settings_info",
-      data: new FormData($(this)[0]),
-      cache: false,
-      contentType: false,
-      processData: false,
-      method: "POST",
-      type: "POST",
-      success: function (resp) {
-        if (resp == 1) {
-          // alert_toast("Data successfully saved",'success')
-          location.reload();
-        } else {
-          $("#msg").html(
-            '<div class="alert alert-danger err_msg">An Error occured</div>'
-          );
-          end_loader();
-        }
-      },
-    });
-  });
+  // $("#system-frm").submit(function (e) {
+  //   e.preventDefault();
+  //   start_loader();
+  //   if ($(".err_msg").length > 0) $(".err_msg").remove();
+  //   $.ajax({
+  //     url: _base_url_ + "classes/SystemSettings.php?f=update_settings_info",
+  //     data: new FormData($(this)[0]),
+  //     cache: false,
+  //     contentType: false,
+  //     processData: false,
+  //     method: "POST",
+  //     type: "POST",
+  //     success: function (resp) {
+  //       if (resp == 1) {
+
+  //         location.reload();
+  //       } else {
+  //         $("#msg").html(
+  //           '<div class="alert alert-danger err_msg">An Error occured</div>'
+  //         );
+  //         end_loader();
+  //       }
+  //     },
+  //   });
+  // });
 });
