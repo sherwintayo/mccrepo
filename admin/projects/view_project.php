68b6b3e12fe30f005925b6006a94590722f7b5d1
sherwintayo <sherwintayo08@gmail.com>
2024-12-14 05:33:48 +0800

    .

--- admin/projects/view_project.php
+++ admin/projects/view_project.php
@@ -1,16 +1,16 @@
-<?php 
-if(isset($_GET['id']) && $_GET['id'] > 0){
+<?php
+if (isset($_GET['id']) && $_GET['id'] > 0) {
     $qry = $conn->query("SELECT a.* FROM `archive_list` a where a.id = '{$_GET['id']}'");
-    if($qry->num_rows){
-        foreach($qry->fetch_array() as $k => $v){
-            if(!is_numeric($k))
-            $$k = $v;
+    if ($qry->num_rows) {
+        foreach ($qry->fetch_array() as $k => $v) {
+            if (!is_numeric($k))
+                $$k = $v;
         }
     }
     $submitted = "N/A";
-    if(isset($student_id)){
+    if (isset($student_id)) {
         $student = $conn->query("SELECT * FROM student_list where id = '{$student_id}'");
-        if($student->num_rows > 0){
+        if ($student->num_rows > 0) {
             $res = $student->fetch_array();
             $submitted = $res['email'];
         }
@@ -18,62 +18,77 @@ if(isset($_GET['id']) && $_GET['id'] > 0){
 }
 ?>
 <style>
-    #document_field{
-        min-height:80vh
+    #document_field {
+        min-height: 80vh
     }
 </style>
+<link rel="stylesheet" href="<?php echo base_url ?>myStyles/projectdetails.css?v=<?php echo time(); ?>">
+
 <div class="content py-4">
     <div class="col-12">
-        <div class="card card-outline card-primary shadow rounded-0">
+        <div class="card">
             <div class="card-header">
                 <h3 class="card-title">
-                    Archive - <?= isset($archive_code) ? $archive_code : "" ?>
+                    Project Details
                 </h3>
             </div>
             <div class="card-body rounded-0">
                 <div class="container-fluid">
-                    <h2><b><?= isset($title) ? $title : "" ?></b></h2>
-                    <small class="text-muted">Submitted by <b class="text-info"><?= $submitted ?></b> on  <?= date("F d, Y h:i A",strtotime($date_created)) ?></small>
-                    <?php if(isset($student_id) && $_settings->userdata('login_type') == "2" && $student_id == $_settings->userdata('id')): ?>
-                        <div class="form-group">
-                            <a href="./?page=submit-archive&id=<?= isset($id) ? $id : "" ?>" class="btn btn-flat btn-default bg-navy btn-sm"><i class="fa fa-edit"></i> Edit</a>
-                            <button type="button" data-id = "<?= isset($id) ? $id : "" ?>" class="btn btn-flat btn-danger btn-sm delete-data"><i class="fa fa-trash"></i> Delete</button>
-                        </div>
-                    <?php endif; ?>
-                    <hr>
                     <center>
-                        <img src="<?= validate_image(isset($banner_path) ? $banner_path : "") ?>" alt="Banner Image" id="banner-img" class="img-fluid border bg-gradient-dark">
+                        <img src="<?= validate_image(isset($banner_path) ? htmlspecialchars($banner_path) : "") ?>"
+                            alt="Banner Image" id="banner-img" class="img-fluid border bg-gradient-dark">
                     </center>
-                    <fieldset>
-                        <legend class="text-navy">Project Year:</legend>
-                        <div class="pl-4"><large><?= isset($year) ? $year : "----" ?></large></div>
-                    </fieldset>
+                    <div class="title">
+                        <h2><b><?= isset($title) ? htmlspecialchars($title) : "" ?></b></h2>
+                        <p>Archive Code - <?= isset($archive_code) ? htmlspecialchars($archive_code) : "" ?></p>
+                        <p class="text-muted">Submitted by <b class="text-info"><?= htmlspecialchars($submitted) ?></b>
+                            on <?= isset($date_created) ? date("F d, Y h:i A", strtotime($date_created)) : "" ?></p>
+                    </div>
+
                     <fieldset>
                         <legend class="text-navy">Abstract:</legend>
-                        <div class="pl-4"><large><?= isset($abstract) ? html_entity_decode($abstract) : "" ?></large></div>
+                        <div class="pl-4">
+                            <large><?= isset($abstract) ? html_entity_decode($abstract) : "" ?></large>
+                        </div>
                     </fieldset>
+
                     <fieldset>
                         <legend class="text-navy">Members:</legend>
-                        <div class="pl-4"><large><?= isset($members) ? html_entity_decode($members) : "" ?></large></div>
+                        <div class="pl-4">
+                            <large><?= isset($members) ? html_entity_decode($members) : "" ?></large>
+                        </div>
                     </fieldset>
+
                     <fieldset>
-						<legend class="text-navy">Project Files:</legend>
-						
-						<a class="btn btn-success" href="<?php echo base_url.'uploads/files/Files-'.$id.'.zip' ?>">Download Project files</a>
-						
-					</fieldset>
-					<fieldset>
-						 <legend class="text-navy">View SQL file:</legend>
-						 <div class="pl-4">
-							<textarea id="summernote" class="form-control form-control-border summernote" readonly>
-								<?= isset($sql_path) ? nl2br(file_get_contents(html_entity_decode(base_url.$sql_path))) : "" ?>
-							</textarea>
-						 </div>
-					</fieldset>
+                        <legend class="text-navy">Project Year:</legend>
+                        <div class="pl-4">
+                            <large><?= isset($year) ? htmlspecialchars($year) : "----" ?></large>
+                        </div>
+                    </fieldset>
+
+                    <fieldset>
+                        <legend class="text-navy">Project Files:</legend>
+                        <div class="pl-4">
+                            <a class="btn btn-success"
+                                href="<?php echo base_url . 'uploads/files/Files-' . $id . '.zip' ?>"><i
+                                    class="fa fa-download"></i> Download Project Files</a>
+                        </div>
+                    </fieldset>
+
+                    <fieldset>
+                        <legend class="text-navy">SQL File:</legend>
+                        <div class="pl-4">
+                            <textarea id="summernote" class="form-control form-control-border summernote" readonly>
+                                <?= isset($sql_path) ? nl2br(file_get_contents(html_entity_decode(base_url . $sql_path))) : "" ?>
+                            </textarea>
+                        </div>
+                    </fieldset>
+
                     <fieldset>
                         <legend class="text-navy">Project Document:</legend>
                         <div class="pl-4">
-                            <iframe src="<?= isset($document_path) ? base_url.$document_path : "" ?>" frameborder="0" id="document_field" class="text-center w-100">Loading Document ...</iframe>
+                            <iframe src="<?= isset($document_path) ? base_url . $document_path : "" ?>" frameborder="0"
+                                id="document_field" class="text-center w-100">Loading Document...</iframe>
                         </div>
                     </fieldset>
                 </div>
@@ -82,35 +97,35 @@ if(isset($_GET['id']) && $_GET['id'] > 0){
     </div>
 </div>
 <script>
-    $(function(){
-        $('.delete-data').click(function(){
-            _conf("Are you sure to delete <b>Archive-<?= isset($archive_code) ? $archive_code : "" ?></b>","delete_archive")
+    $(function () {
+        $('.delete-data').click(function () {
+            _conf("Are you sure to delete <b>Archive-<?= isset($archive_code) ? $archive_code : "" ?></b>", "delete_archive")
         })
-		$('.summernote').summernote({
+        $('.summernote').summernote({
             height: 200
         })
-		$("#summernote").summernote("disable");
+        $("#summernote").summernote("disable");
     })
-    function delete_archive(){
-		start_loader();
-		$.ajax({
-			url:_base_url_+"classes/Master.php?f=delete_archive",
-			method:"POST",
-			data:{id: "<?= isset($id) ? $id : "" ?>"},
-			dataType:"json",
-			error:err=>{
-				console.log(err)
-				alert_toast("An error occured.",'error');
-				end_loader();
-			},
-			success:function(resp){
-				if(typeof resp== 'object' && resp.status == 'success'){
-					location.replace("./");
-				}else{
-					alert_toast("An error occured.",'error');
-					end_loader();
-				}
-			}
-		})
-	}
+    function delete_archive() {
+        start_loader();
+        $.ajax({
+            url: _base_url_ + "classes/Master.php?f=delete_archive",
+            method: "POST",
+            data: { id: "<?= isset($id) ? $id : "" ?>" },
+            dataType: "json",
+            error: err => {
+                console.log(err)
+                alert_toast("An error occured.", 'error');
+                end_loader();
+            },
+            success: function (resp) {
+                if (typeof resp == 'object' && resp.status == 'success') {
+                    location.replace("./");
+                } else {
+                    alert_toast("An error occured.", 'error');
+                    end_loader();
+                }
+            }
+        })
+    }
 </script>
