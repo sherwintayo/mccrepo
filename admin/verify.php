1912989bbc251bf7b48876845c5f2365eea3ddd3
sherwintayo <sherwintayo08@gmail.com>
2024-12-14 18:44:43 +0800

    /

--- admin/verify.php
+++ admin/verify.php
@@ -34,7 +34,7 @@ if (isset($_GET['token'])) {
     $_SESSION['userdata']['login_type'] = 1; // Set login type as admin
 
     // Redirect to the admin dashboard
-    header("Location: ../admin/index.php");
+    header("Location: ../admin/index");
     exit; // Ensure no further execution
   } else {
     echo "Invalid or expired token.";
