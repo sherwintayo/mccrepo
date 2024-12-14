f128981fe9fb7ccc311546e9149b6456c6acf3e0
sherwintayo <sherwintayo08@gmail.com>
2024-12-14 18:42:54 +0800

    .

--- admin/inc/sess_auth.php
+++ admin/inc/sess_auth.php
@@ -11,7 +11,7 @@ $link .= "://";
 $link .= $_SERVER['HTTP_HOST'];
 $link .= $_SERVER['REQUEST_URI'];
 if (!isset($_SESSION['userdata']) && !strpos($link, 'login') && !strpos($link, 'register.php')) {
-    redirect('admin/');
+    redirect('admin/index');
 }
 if (isset($_SESSION['userdata']) && strpos($link, 'login.php')) {
     redirect('admin/index.php');
