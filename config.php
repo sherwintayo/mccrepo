69439957460a6d14f14d7672e5fb2d2618c38159
sherwintayo <sherwintayo08@gmail.com>
2024-12-15 15:27:15 +0800

    .

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ header("Strict-Transport-Security: max-age=31536000; includeSubDomains; preload"
 
 header("X-Content-Type-Options: nosniff");
 
-header("X-Frame-Options: DENY");
+// header("X-Frame-Options: DENY");
 
 header("X-XSS-Protection: 1; mode=block");
 
